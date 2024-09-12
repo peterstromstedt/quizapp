f2fbcd5afaf0985d658fc398caddc1968014004f
peterstromstedt <110103285+peterstromstedt@users.noreply.github.com>
2024-09-12 10:30:39 +0200

    version 1.0

---
+++ extrastuffadded.md
@@ -0,0 +1,2 @@
+we added gsap, for help with the animations.
+npm install gsap
---
+++ src/components/QuizHeader.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const {questionStatus, barPrecentage} = defineProps(["questionStatus", "barPrecentage"]);
+</script>
+
+<template>
+    <header>
+        <h4>Question {{ questionStatus }}</h4>
+        <div class="bar">
+            <div class="completion" :style="{width: barPrecentage}"></div>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        margin-top: 20px;
+    }
+
+    header h4 {
+        font-size: 30px;
+    }
+
+    .bar {
+        width: 100%;
+        height: 50px;
+        border: 3px solid bisque;
+    }
+
+    .completion {
+        height: 100%;
+        width: 0%;
+        background-color: bisque;
+    }
+</style>
